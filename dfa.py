d79cc052c4fcd201e076a91b299d78e71ea129bb
Francesco-Biscaccia <francesco.biscaccia.carrara@gmail.com>
2023-10-16 17:00:24 +0200

    Version of DFA that accept any alphabet.

--- dfa.py
+++ dfa.py
@@ -1,18 +1,31 @@
 from state import State
 
+
 class DFA:
     def __init__(self,in_file):
         self.states=[]
         self.labels=""
         f = open(in_file, "r")
+        self.alphabet = f.readline()[2:-1]
         for line in f:
+            new_states={}
+            i=0
+            for symbol in self.alphabet:
+                new_states.update({symbol:line[2+i]})
+                i+=1
             self.labels+=line[0]
             self.states.append(State(line[0],
                                  True if line[1]=="*" else False,
-                                 {"0":line[2],"1":line[3]}
+                                 new_states
                                  ))
+            print(new_states)
+        
 
     def check(self,word):
+        for c in word:
+            if not c in self.alphabet: 
+                return False
+        
         curr_s = self.states[0]
         for c in word:
             curr_s = self.states[self.labels.find(curr_s.compute(c))]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dfa import DFA
 
 d = DFA("./test/input_0.txt")
-s="1010101"
+s="a010001"
 if d.check(s):
     print("Your DFA accept the string "+s)
 else: 
